3c547d7a4e8ac5ee194a084bac960d7211927b18
Ángel <angel@dinaup.com>
2024-04-07 22:41:52 +0200

    xxx

---
+++ LogTest/Program.vb
@@ -0,0 +1,55 @@
+
+Module Program
+    Sub Main(args As String())
+        ' Configuración de Serilog
+        dinaup.logs.Initialize("LogTest", "0.1", logFilePath:="log/log.txt", mmWebHook:="matte", elasticUrl:="http://localhost:9200", elasticPrefix:="")
+
+
+        ' Generar logs de prueba
+        dinaup.logs.Information("Iniciando pruebas de Serilog.")
+
+        ' Llamadas a funciones de prueba
+        GenerarLogsTexto()
+        GenerarLogsNumeros()
+        GenerarLogsExcepciones()
+
+
+        Using dinaup.logs.IniContext("sync")
+            dinaup.logs.Log.Information("Log de texto número {NumeroLog}", 2)
+        End Using
+
+
+        dinaup.logs.Log.Information("Pruebas de Serilog finalizadas.")
+        dinaup.logs.CloseAndFlush()
+        Console.ReadKey()
+
+    End Sub
+
+    ' Función para generar logs de texto
+    Sub GenerarLogsTexto()
+        For i As Integer = 1 To 1000
+            dinaup.logs.Log.Information("Log de texto número {NumeroLog}", i)
+        Next
+    End Sub
+
+    ' Función para generar logs con números
+    Sub GenerarLogsNumeros()
+        Dim rand As New Random()
+        For i As Integer = 1 To 1000
+            dinaup.logs.Log.Debug("Log con número aleatorio {NumeroAleatorio}", rand.Next())
+        Next
+    End Sub
+
+    ' Función para simular y registrar excepciones
+    Sub GenerarLogsExcepciones()
+        For i As Integer = 1 To 100
+            Try
+                Throw New InvalidOperationException("Excepción de prueba número " & i.ToString())
+            Catch ex As Exception
+                dinaup.logs.Log.Error(ex, "Se capturó una excepción")
+            End Try
+        Next
+    End Sub
+
+End Module
+    
---
+++ src/Logger/UserLogger.vb
@@ -0,0 +1,19 @@
+﻿Public Class UserLogger
+
+
+    Public UserName$
+    Public UserID$
+
+
+
+
+
+
+
+
+
+    Sub New(_UserName$, _userId$)
+        Me.UserName = _UserName
+        Me.UserID = _userId
+    End Sub
+End Class
